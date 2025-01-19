b270548f0a0f5ec0afae9d700e26ffa76f0ebd9b
Krishna Gupta <krishna.gupta2022@sait.ac.in>
2025-01-19 15:32:27 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/Placeorder/PlaceOrder'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/PlaceOrder' element={<PlaceOrder />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
